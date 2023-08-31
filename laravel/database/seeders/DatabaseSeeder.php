a6a5a9a9aa4ae4baf468cf115ac4b8f8b111c12b
Andre Salvador <andrepasalvador@gmail.com>
2023-08-31 20:20:15 -0300

    criando seeders

--- laravel/database/seeders/DatabaseSeeder.php
+++ laravel/database/seeders/DatabaseSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +15,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'user' => 'test',
+            'password' => Hash::make(123),
+            'token_dashboard' => '123',
+            'admin' => 1
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\Grupo::create([
+            'titulo' => 'Geral',
+            'tipo' => 'notes',
+            'user_id' => 1,
+            'default' => 1
+        ]);
     }
 }
